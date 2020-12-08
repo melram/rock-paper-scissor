b664b94ddde4991647f4b13b8223efea7207a5c5
PanchoLoco <franciscoapguillen@gmail.com>
2020-12-08 20:50:06 -0300

    listo sin css y sin totales

--- index.js
+++ index.js
@@ -2,9 +2,52 @@ Vue.config.devtools = true;
 var AppSPR = new Vue({
   el: "#app",
   data: {
-    answer: "Prueba",
+    winner: "",
+    results: [],
+    player: "",
+    name: "",
+    disabled: 0,
+    cpuoption: "",
+    options: ["rock", "paper", "scissors"],
   },
-  watch: {},
-  created: function () {},
-  methods: {},
+  methods: {
+    playgame(player) {
+      computer = this.options[Math.floor(Math.random() * 3)];
+      switch (player + computer) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+          this.winner = "El Ganador es:" + this.name;
+          this.results.push({
+            player: player,
+            computer: computer,
+            winner: this.name,
+          });
+          break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+          this.winner = "El Ganador es: La computadora";
+          this.results.push({
+            player: player,
+            computer: computer,
+            winner: "Computer",
+          });
+          break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+          this.winner = "Empate sigamos jugando...";
+          this.results.push({
+            player: player,
+            computer: computer,
+            winner: "Empate",
+          });
+          break;
+      }
+    },
+    saveplayer() {
+      this.disabled = 1;
+    },
+  },
 });
